45b22f94b61814b198312b82a08d9286bf7af22d
Diego Pacori <dpacoria@unsa.edu.pe>
2024-02-09 22:20:06 -0500

    Se hizo validaciones manuales en formik

--- 04-pattern-compound-component/src/03-forms/pages/FormikBasicPage.tsx
+++ 04-pattern-compound-component/src/03-forms/pages/FormikBasicPage.tsx
@@ -1,18 +1,54 @@
-import { useFormik } from 'formik';
+import { FormikErrors, useFormik } from 'formik';
 import '../styles/styles.css';
+
+interface FormValues {
+    firstName: string,
+    lastName: string,
+    email: string,
+}
+
+const formInit: FormValues = {
+    firstName: '',
+    lastName: '',
+    email: ''
+};
 
 export const FormikBasicPage = () => {
 
+    const validate = ({email, firstName, lastName}: FormValues) => {
+        
+        const errors:FormikErrors<FormValues> = {};
+
+        if(!firstName) {
+            errors.firstName = 'Required';
+        }else if(firstName.length > 15) {
+            errors.firstName = 'Must be 15 characters or less';
+        }
+
+        if(!lastName) {
+            errors.lastName = 'Required';
+        }else if(lastName.length > 15) {
+            errors.lastName = 'Must be 15 characters or less';
+        }
+
+        if (!email) {
+            errors.email = 'Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+            errors.email = 'Invalid email address';
+        }
+
+
+        return errors;
+    };
+
     const {handleChange, values, handleSubmit} = useFormik({
-        initialValues: {
-            firstName: '',
-            lastName: '',
-            email: '',
-        },
+        initialValues: formInit,
         onSubmit: (values) => {
             console.log(values);
-        }
+        },
+        validate
     });
+
     
     return (
         <div>
